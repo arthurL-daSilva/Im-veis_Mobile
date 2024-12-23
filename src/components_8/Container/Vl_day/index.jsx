aabec672ac877724e1d7fd06477a2c266cfbf5f5
arthurL-daSilva <arthurleal723@gmail.com>
2024-12-23 12:39:02 -0300

    Input "Valor da diária" agora formata com valores monetários

--- src/components_8/Container/Vl_day/index.jsx
+++ src/components_8/Container/Vl_day/index.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react'
 import { Day } from './style'
 
-function Home(){ 
+function Home(){
+
+    const [valor, setValor] = useState("")
+    const InputEvent = (event) => {
+        const inputValor = event.target.value
+        const valorNumerico = inputValor.replace(/[^\d,.]/g, "")
+        setValor(valorNumerico ? `R$ ${valorNumerico}` : "")
+    }
+    
 
     return(
         <Day>
             <p>Diária</p>
-            <input type="number" placeholder='Valor da diária' />
+            <input value={valor} type="text" onChange={InputEvent} placeholder='Valor da diária' />
         </Day>
     )
 }
